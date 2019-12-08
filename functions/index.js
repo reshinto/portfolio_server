687634e32ab1609460af4f5e59de6093cc0c7241
reshinto <terencekong2002@gmail.com>
2019-12-08 17:01:10 +0800

    Add basic read feature

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,23 @@
 const functions = require('firebase-functions');
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello World!");
+admin.initializeApp();
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+// Read
+exports.getPortfolios = functions.https.onRequest((req, res) => {
+  if (req.method !== "GET") {
+    return res.status(400).json({error: "Invalid request method!"});
+  }
+  admin.firestore().collection("portfolios").get()
+    .then(data => {
+      let portfolios;
+      data.forEach(portfolio => {
+        portfolios = portfolio.data();
+      });
+      return res.json(portfolios)
+    })
+    .catch(err => console.error(err));
 });
